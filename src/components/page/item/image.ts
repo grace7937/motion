1f164cbbf5c3387a7c13a30e739880da0951818f
grace7937 <eunchong.k153@gmail.com>
2021-10-06 22:11:09 +0900

    Modify image.ts

--- src/components/page/item/image.ts
+++ src/components/page/item/image.ts
@@ -1,21 +1,16 @@
-export class ImageComponent {
-    private element: HTMLElement;
-    constructor(title: string, url: string) {
-      const template = document.createElement('template');
-      template.innerHTML = `<section class="image">
-    <div class="image__holder"><img class="image__thumbnail"></div>
-    <p class="image__title"></p>
-  </section>`;
-      this.element = template.content.firstElementChild! as HTMLElement;
-  
-      const imageElement = this.element.querySelector('.image__thumbnail')! as HTMLImageElement;
-      imageElement.src = url;
-      imageElement.alt = title;
-  
-      const titleElement = this.element.querySelector('.image__title')! as HTMLParagraphElement;
-      titleElement.textContent = title;
-    }
-    attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
-      parent.insertAdjacentElement(position, this.element);
-    }
+import { BaseComponent } from './../../component.js';
+
+export class ImageComponent extends BaseComponent<HTMLElement> {
+  constructor(title: string, url: string) {
+    super(`<section class="image">
+            <div class="image__holder"><img class="image__thumbnail"></div>
+            <h2 class="image__title"></h2>
+          </section>`);
+
+    const imageElement = this.element.querySelector('.image__thumbnail')! as HTMLImageElement;
+    imageElement.src = url;
+    imageElement.alt = title;
+    const titleElement = this.element.querySelector('.image__title')! as HTMLParagraphElement;
+    titleElement.textContent = title;
   }
+}
