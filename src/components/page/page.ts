6b6147a7a4f8e3a5bf92d085d9cd085e2c342d38
grace7937 <eunchong.k153@gmail.com>
2021-10-10 18:50:07 +0900

    Add video.ts

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -1,14 +1,7 @@
-export class PageComponent {
-    private element: HTMLUListElement;
-    constructor() {
-      this.element = document.createElement('ul');
-      this.element.setAttribute('class', 'page');
-      this.element.textContent = 'This is PageComponent';
-    }
-    
-    attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
-      parent.insertAdjacentElement(position, this.element);
-    }
+import { BaseComponent } from './../component.js';
+
+export class PageComponent extends BaseComponent<HTMLUListElement> {
+  constructor() {
+    super('<ul class="page">This is Page Componenet!</ul>');
   }
-
-
+}
