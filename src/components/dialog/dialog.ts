185804fa55ade0433059be0ddac105a5f3df5ceb
grace7937 <eunchong.k153@gmail.com>
2021-10-20 23:14:48 +0900

    Refactor

--- src/components/dialog/dialog.ts
+++ src/components/dialog/dialog.ts
@@ -1,13 +1,21 @@
 import { Composable } from '../page/page.js';
 import { BaseComponent, Component } from './../component.js';
-
 type OnCloseListener = () => void;
 type OnSubmitListener = () => void;
+
+export interface MediaData {
+  readonly title: string;
+  readonly url: string;
+}
+
+export interface TextData {
+  readonly title: string;
+  readonly body: string;
+}
 
 export class InputDialog extends BaseComponent<HTMLElement> implements Composable {
   closeListener?: OnCloseListener;
   submitListener?: OnSubmitListener;
-
   constructor() {
     super(`<dialog class="dialog">
           <div class="dialog__container">
@@ -20,21 +28,8 @@ export class InputDialog extends BaseComponent<HTMLElement> implements Composabl
     closeBtn.onclick = () => {
       this.closeListener && this.closeListener();
     };
-
     const submitBtn = this.element.querySelector('.dialog__submit')! as HTMLElement;
     submitBtn.onclick = () => {
       this.submitListener && this.submitListener();
     };
   }
-
-  setOnCloseListenr(listener: OnCloseListener) {
-    this.closeListener = listener;
-  }
-  setOnSubmitListenr(listener: OnSubmitListener) {
-    this.submitListener = listener;
-  }
-  addChild(child: Component) {
-    const body = this.element.querySelector('#dialog__body')! as HTMLElement;
-    child.attachTo(body);
-  }
-}
