5123d284aba5f04036fd481a2f124c3f966e673d
grace7937 <eunchong.k153@gmail.com>
2021-10-24 21:36:51 +0900

    Refactor

--- src/components/page/item/video.ts
+++ src/components/page/item/video.ts
@@ -3,16 +3,14 @@ export class VideoComponent extends BaseComponent<HTMLElement> {
   constructor(title: string, url: string) {
     super(`<section class="video">
             <div class="video__player"><iframe class="video__iframe"></iframe></div>
-            <h3 class="video__title"></h3>
+            <h3 class="page-item__title video__title"></h3>
         </section>`);
 
     const iframe = this.element.querySelector('.video__iframe')! as HTMLIFrameElement;
     iframe.src = this.convertToEmbeddedURL(url);
-
     const titleElement = this.element.querySelector('.video__title')! as HTMLHeadingElement;
     titleElement.textContent = title;
   }
-
   // input
   // https://www.youtube.com/watch?v=K3-jG52XwuQ
   // https://youtu.be/K3-jG52XwuQ
