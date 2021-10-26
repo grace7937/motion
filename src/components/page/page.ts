60b6f714e55ca4ea9a91cc623c6c936853db0b48
grace7937 <eunchong.k153@gmail.com>
2021-10-26 22:21:07 +0900

    Modify page.ts

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -3,20 +3,17 @@ export interface Composable {
   addChild(child: Component): void;
 }
 type OnCloseListener = () => void;
-
 interface SectionContainer extends Component, Composable {
   setOnCloseListener(listener: OnCloseListener): void;
 }
-
 type SectionContainerConstructor = {
   new (): SectionContainer;
 };
-
 export class PageItemComponent extends BaseComponent<HTMLElement> implements SectionContainer {
   private closeListener?: OnCloseListener;
 
   constructor() {
-    super(`<li class="page-item">
+    super(`<li draggable="true" class="page-item">
             <section class="page-item__body"></section>
             <div class="page-item__controls">
               <button class="close">&times;</button>
@@ -26,7 +23,20 @@ export class PageItemComponent extends BaseComponent<HTMLElement> implements Sec
     closeBtn.onclick = () => {
       this.closeListener && this.closeListener();
     };
+    this.element.addEventListener('dragstart', (event: DragEvent) => {
+      this.onDragStart(event);
+    });
+    this.element.addEventListener('dragend', (event: DragEvent) => {
+      this.onDragEnd(event);
+    });
   }
+  onDragStart(event: DragEvent) {
+    console.log('dragstart', event);
+  }
+  onDragEnd(event: DragEvent) {
+    console.log('dragend', event);
+  }
+
   addChild(child: Component) {
     const container = this.element.querySelector('.page-item__body')! as HTMLElement;
     child.attachTo(container);
@@ -38,6 +48,21 @@ export class PageItemComponent extends BaseComponent<HTMLElement> implements Sec
 export class PageComponent extends BaseComponent<HTMLUListElement> implements Composable {
   constructor(private pageItemConstructor: SectionContainerConstructor) {
     super('<ul class="page"></ul>');
+    this.element.addEventListener('dragover', (event: DragEvent) => {
+      this.onDragOver(event);
+    });
+    this.element.addEventListener('drop', (event: DragEvent) => {
+      this.onDrop(event);
+    });
+  }
+
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+    console.log('onDragOver');
+  }
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    console.log('onDrop');
   }
 
   addChild(section: Component) {
